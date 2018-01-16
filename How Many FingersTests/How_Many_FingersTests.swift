3c2f411cde22d901a41a65108fd92690f68fc055
selveus1 <fs031090@gmail.com>
2018-01-16 15:58:23 -0500

    Initial Commit

---
+++ How Many FingersTests/How_Many_FingersTests.swift
@@ -0,0 +1,36 @@
+//
+//  How_Many_FingersTests.swift
+//  How Many FingersTests
+//
+//  Created by Athena on 1/16/18.
+//  Copyright © 2018 Sheena Elveus. All rights reserved.
+//
+
+import XCTest
+@testable import How_Many_Fingers
+
+class How_Many_FingersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
